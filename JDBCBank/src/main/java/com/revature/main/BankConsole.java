4c5d7823963127ef674f4d08d762759c34c1cfca
tadegbenro <tomi.adegbenro@gmail.com>
2019-09-20 14:00:00 -0400

    Changes in JDBCBank app2 final before presentation

--- JDBCBank/src/main/java/com/revature/main/BankConsole.java
+++ JDBCBank/src/main/java/com/revature/main/BankConsole.java
@@ -9,33 +9,7 @@ import com.revature.main.AccessOperations;
 
 public class BankConsole {
 	public static void main(String[] args) {
-		/*
-		int option=0;
-		Scanner scanner = new Scanner(System.in);
-		System.out.println("Welcome to JDBC Bank \n"
-				+ "Please Enter Menu #: \n"
-				+ "User Login: \t 1\n"
-				+ "Register: \t 2");
-		try{
-		option= scanner.nextInt();
-		}
-		catch(InputMismatchException e) {
-			System.out.println("Try again");
-			//scanner.close();
-					
-		}
-		//System.out.println("Please Enter Menu #: \n"
-		//		+ "User Login: \t 1\n"
-		//		+ "Register: \t 2");
 		
-		switch(option) {
-		case 1: 
-			Login.getLoginCredentials();
-			break;
-		case 2:
-			Registration.registerCredentials();
-			break;
-		}*/
 		BankMainConsole.displayMainMenu();
 		
 	}
